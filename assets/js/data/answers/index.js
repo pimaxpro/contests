010f6892a540c65a15fd3874a891a66dec4ea279
pimaxpro <dducpimax@gmail.com>
2026-07-30 16:05:54 +0700

    Delete assets directory

--- assets/js/data/answers/index.js
+++
@@ -1,4 +0,0 @@
-/* =========================================================
-   ANSWER BANK CENTRAL REGISTRY (FILE KHỞI TẠO TỔNG)
-========================================================= */
-window.EXAM_ANSWERS_BANK = window.EXAM_ANSWERS_BANK || {};
