ef451e4e3a58a467e25d1ceb086729d2764c39d6
pimaxpro <dducpimax@gmail.com>
2026-07-30 15:31:22 +0700

    Create PMXST26-FALLT1.js

---
+++ assets/js/data/answers/tournament/PMXST26-FALLT1.js
@@ -0,0 +1,15 @@
+/* =========================================================
+   ĐÁP ÁN: TSABK TOURNAMENT - PMXST26-FALLT1
+========================================================= */
+window.EXAM_ANSWERS_BANK = window.EXAM_ANSWERS_BANK || {};
+
+window.EXAM_ANSWERS_BANK["PMXST26-FALLT1"] = {
+  examTitle: "PMXST26−FALLT1 (Tuần 1)",
+  answers: {
+    1: "C",
+    2: "A",
+    3: { "a": "Đúng", "b": "Đúng", "c": "Sai", "d": "Đúng" },
+    4: "D",
+    5: "75"
+  }
+};
